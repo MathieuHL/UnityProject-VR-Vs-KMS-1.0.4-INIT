4dd48131f573d0bef74b222f7be0559d65cabbad
Roméo <romeo.bernat@gmail.com>
2022-10-11 15:13:35 +0200

    Cam Correction

---
+++ Assets/Standard Assets/Cameras/Scripts/ProtectFromParentPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtectFromParentPosition : MonoBehaviour
+{
+    public Transform self;
+
+    private void Awake()
+    {
+        self.SetParent(null, true);
+    }
+}

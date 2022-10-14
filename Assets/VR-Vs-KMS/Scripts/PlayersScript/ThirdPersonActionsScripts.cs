216bdac0e69c993870deb1caf6415b073027eddc
MathieuHL <mathieu.hoaraulevet@viacesi.fr>
2022-10-14 15:19:28 +0200

    up

--- Assets/VR-Vs-KMS/Scripts/PlayersScript/ThirdPersonActionsScripts.cs
+++ Assets/VR-Vs-KMS/Scripts/PlayersScript/ThirdPersonActionsScripts.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonActionsScripts : MonoBehaviour
+public class ThirdPersonActionsScripts : MonoBehaviourPunCallbacks
 {
     public int tpLifePoint;
     public string tpColorShoot;
@@ -13,4 +14,17 @@ public class ThirdPersonActionsScripts : MonoBehaviour
         tpColorShoot = GameManager.Instance.gameSetting.ColorShotKMS;
     }
 
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            Vector3 pos = transform.localPosition;
+            stream.Serialize(ref pos);
+        }
+        else
+        {
+            Vector3 pos = Vector3.zero;
+            stream.Serialize(ref pos);  // pos gets filled-in. must be used somewhere
+        }
+    }
 }
--- Assets/VR-Vs-KMS/Scripts/VR/VRPlayerScript.cs
+++ Assets/VR-Vs-KMS/Scripts/VR/VRPlayerScript.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
-public class VRPlayerScript : MonoBehaviour
+public class VRPlayerScript : MonoBehaviourPunCallbacks
 {
     public Transform leftHand, rightHand;
     public GameObject ballPrefab, shieldGO;
@@ -19,12 +20,11 @@ public class VRPlayerScript : MonoBehaviour
     {
         if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
-            var shot = Instantiate(ballPrefab, rightHand, false);
+            var shot = Instantiate(ballPrefab, rightHand.position, rightHand.rotation);
             shot.transform.localPosition = new Vector3(0, 0, 0);
 
             var shotRb = shot.GetComponent<Rigidbody>();
             shotRb.velocity = (-rightHand.up+rightHand.forward)*25f;
-
         }
 
         if (SteamVR_Actions.default_GrabPinch.GetStateDown(SteamVR_Input_Sources.LeftHand))
@@ -35,6 +35,20 @@ public class VRPlayerScript : MonoBehaviour
         if (SteamVR_Actions.default_GrabPinch.GetStateUp(SteamVR_Input_Sources.LeftHand))
         {
             shieldGO.SetActive(false);
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            Vector3 pos = transform.localPosition;
+            stream.Serialize(ref pos);
+        }
+        else
+        {
+            Vector3 pos = Vector3.zero;
+            stream.Serialize(ref pos);  // pos gets filled-in. must be used somewhere
         }
     }
 }
